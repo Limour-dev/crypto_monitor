a57d1bc613a1457b123cb9f83cb24b629b1f6cda
Limour <limour@limour.top>
2025-09-04 21:18:09 +0800

    fix: 调整通知格式，突出重点

--- strategy/ETHUSDT/__init__.py
+++ strategy/ETHUSDT/__init__.py
@@ -4,7 +4,7 @@ from strategy.common import f_log_regression, ta_stdev
 from itertools import chain
 
 key_levels = [
-    4117, 4215, 4262, 4306, 4349, 4387, 4453, 4496
+    4117, 4215, 4262, 4306, 4349, 4387, 4453, 4496, 4546
 ]
 
 key_levels_last_notify = 0
@@ -12,6 +12,16 @@ notify_interval = 59999
 
 lg_last_c = 0
 lg = [0, 0]
+
+def buy_vs_sell_v(bar):
+    return (bar.taker * 2 - bar.v) / bar.v * 100
+def buy_vs_sell_p(bar):
+    return (bar.c - bar.o) / bar.c * 1000
+def buy_vs_sell(bars):
+    res = f'{buy_vs_sell_p(bars[-3]):.2f}\t{buy_vs_sell_v(bars[-3]):.2f}\n'
+    res += f'{buy_vs_sell_p(bars[-2]):.2f}\t{buy_vs_sell_v(bars[-2]):.2f}\n'
+    res += f'{buy_vs_sell_p(bars[-1]):.2f}\t{buy_vs_sell_v(bars[-1]):.2f}'
+    return res
 
 def p_bar(bar, symbols):
     global key_levels_last_notify, lg_last_c
@@ -31,5 +41,5 @@ def p_bar(bar, symbols):
         tmp = crossover(bar, level)
         if tmp:
             if bar.ts - key_levels_last_notify >= notify_interval:
-                wechat_push(f'ETHUSDT {bar} crossover {level} {tmp}')
+                wechat_push(f'ETHUSDT {level:.2f} {bar} {tmp} \n {buy_vs_sell(bars_1h)}')
                 key_levels_last_notify = bar.ts
