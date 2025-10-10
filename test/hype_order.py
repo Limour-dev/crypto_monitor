a3da5b0ace4a45995115fca72b3f58039fc8beb6
Limour <limour@limour.top>
2025-10-10 22:16:58 +0800

    fix: 防止把网线不讲武德

--- test/hype_order.py
+++ test/hype_order.py
@@ -1,4 +1,4 @@
-import sys, os, time, random
+import sys, os, time, random, traceback
 fp_root = os.path.dirname(os.path.dirname(__file__))
 if fp_root not in sys.path:
     sys.path.append(fp_root)
@@ -7,19 +7,22 @@ from exchange.hyperliquid.order import sell_min, buy_min, schedule_cancel
 from exchange.hyperliquid.info import l2Book
 all = 0
 while True:
-    ask, bid = l2Book('ETH')
-    if bid[0][0] < 4350:
-        print('skip', bid[0][0])
-        time.sleep(random.randint(20, 30))
-        continue
-    res = sell_min('ETH', max(bid, key=lambda x:x[1])[0])
-    all += 1
-    print(all, res)
-    res = schedule_cancel(30 * 1000)
-    print(res)
-    if all > 30:
-        break
-    time.sleep(random.randint(30, 60))
+    try:
+        ask, bid = l2Book('ETH')
+        if bid[0][0] < 4350:
+            print('skip', bid[0][0])
+            time.sleep(random.randint(20, 30))
+            continue
+        res = sell_min('ETH', max(bid, key=lambda x:x[1])[0])
+        all += 1
+        print(all, res)
+        res = schedule_cancel(30 * 1000)
+        print(res)
+        if all > 30:
+            break
+        time.sleep(random.randint(30, 60))
+    except:
+        traceback.print_last()
 
 if False:
     from exchange.hyperliquid.order import account, address
