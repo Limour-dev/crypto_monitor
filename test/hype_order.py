faadf0025551c8d29d63706331de5637a8e32038
Limour <limour@limour.top>
2025-10-10 17:51:34 +0800

    fix: hyperliquid 简单建仓

--- test/hype_order.py
+++ test/hype_order.py
@@ -1,4 +1,4 @@
-import sys, os, time
+import sys, os, time, random
 fp_root = os.path.dirname(os.path.dirname(__file__))
 if fp_root not in sys.path:
     sys.path.append(fp_root)
@@ -8,17 +8,18 @@ from exchange.hyperliquid.info import l2Book
 all = 0
 while True:
     ask, bid = l2Book('ETH')
-    if bid[-1][0] < 4340:
-        time.sleep(30)
+    if bid[0][0] < 4350:
+        print('skip', bid[0][0])
+        time.sleep(random.randint(20, 30))
         continue
-    res = sell_min('ETH', bid[-1][0])
+    res = sell_min('ETH', max(bid, key=lambda x:x[1])[0])
     all += 1
     print(all, res)
     res = schedule_cancel(30 * 1000)
     print(res)
     if all > 30:
         break
-    time.sleep(60)
+    time.sleep(random.randint(30, 60))
 
 if False:
     from exchange.hyperliquid.order import account, address
